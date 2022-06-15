1d044756ad703898efba6acf475c1c21ab3e86a6
aiexgt <lima14florian14@gmail.com>
2022-06-15 10:45:17 -0600

    Inicio proyecto

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const add = document.getElementById("add");
+const close = document.getElementById("close");
+const nombre = document.getElementById("nombre");
+const semestre = document.getElementById("semestre");
+const credito = document.getElementById("credito");
+const nota = document.getElementById("nota");
